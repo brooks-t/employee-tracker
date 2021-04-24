adc8adf74decf9a2a4d890bfbbee0a34bfc8ca46
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-04-24 13:13:18 -0700

    added employee tracker menu and functions for each choice. currently have added header graphic to each function when chosen. department view is working.

--- index.js
+++ index.js
@@ -9,19 +9,136 @@ const connection = mysql.createConnection({
     database: 'employee_tracker',
   });
 
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log(`success! connected as id ${connection.threadId}`);
+    start();
+  })
+
   const start = () => {
-      inquirer.prompt(
+      inquirer
+      .prompt(
           {
               name: "choice",
               type: "list",
               message: "What would you like to do?",
-              choices: ["Add department", "Add roles", "Add employees", "View departments", "View roles", "View employees", "Update employee roles"]
+              choices: [
+                "Add department",
+                "Add role",
+                "Add employee",
+                "View departments",
+                "View roles",
+                "View employees",
+                "Update employee roles",
+                "Exit"
+            ]
           }
-      ).then(answer => {
-        console.log(`You chose: `, answer.choice)
+      )
+      .then(answer => {
+        switch (answer.choice) {
+            case "Add department":
+                addDepartment();
+                break;
+            
+            case "Add role":
+                addRole();
+                break;
+            
+            case "Add employee":
+                addEmployee();
+                break;
+            
+            case "View departments":
+                viewDepartments();
+                break;
+            
+            case "View roles":
+                viewRoles();
+                break;
+            
+            case "View employees":
+                viewEmployees();
+                break;
+            
+            case "Update employee roles":
+                updateEmployeeRoles();
+                break;
+            
+            case "Exit": // exits but I also get some wierd error
+                console.log("Goodebye!");
+                connection.end();
+                break;
+            
+            default:
+                console.log("Somehow you found a way to break me! Please choose a valid option.");
+                start();
+                break;
+        }
       });
-      connection.end();
   }
+  
+  const addDepartment = () => {
+    console.log("--------------------");
+    console.log("-- ADD DEPARTMENT --");
+    console.log("--------------------");
+    inquirer
+    .prompt({
+        name: "departmentName",
+        type: "input",
+        message: "Please enter the name of the department you would like to add: "
+    })
+    // TODO: THIS IS BROKEN WITH SYTAX
+    .then((answer) => {
+        const addName = "INSERT INTO department (name) VALUES ?";
+        connection.query(addName, {departmentName: answer.departmentName}, (err, res) => {
+            if (err) throw err;
+            console.log(res);
+        });
+    });
+  };
+  
+  const addRole = () => {
+    console.log("--------------");
+    console.log("-- ADD ROLE --");
+    console.log("--------------");
+  };
+  
+  const addEmployee = () => {
+    console.log("------------------");
+    console.log("-- ADD EMPLOYEE --");
+    console.log("------------------");
+  };
+
+  const viewDepartments = () => {
+    console.log("----------------------");
+    console.log("-- VIEW DEPARTMENTS --");
+    console.log("----------------------");
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    })
+  };
+
+  const viewRoles = () => {
+    console.log("----------------");
+    console.log("-- VIEW ROLES --");
+    console.log("----------------");
+  };
+
+  const viewEmployees = () => {
+    console.log("--------------------");
+    console.log("-- VIEW EMPLOYEES --");
+    console.log("--------------------");
+  };
+
+  const updateEmployeeRoles = () => {
+    console.log("-------------------------");
+    console.log("-- UPDATE EMPLOYEE ROLES --");
+    console.log("-------------------------");
+  };
+
+
 
   const afterConnection = () => {
       connection.query('SELECT * FROM employee', (err, res) => {
@@ -30,10 +147,5 @@ const connection = mysql.createConnection({
           connection.end();
       })
   }
-  
-  connection.connect((err) => {
-      if (err) throw err;
-      console.log(`success! connected as id ${connection.threadId}`);
-      start();
-  })
+
 
